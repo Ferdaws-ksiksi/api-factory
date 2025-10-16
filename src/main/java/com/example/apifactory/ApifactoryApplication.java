ea85ee00c309887a0607be57274b74b31342ad79
unknown <ferdawsksiksi2015@gmail.com>
2025-10-16 13:06:47 +0100

    feat: add docker-compose

---
+++ src/main/java/com/example/apifactory/ApifactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.apifactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apifactory/ApifactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apifactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
